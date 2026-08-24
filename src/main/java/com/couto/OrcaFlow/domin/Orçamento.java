0a79db1329544f47886282aa2bce2492f3c1aa20
cauacouto <cauacouto022@gmail.com>
2026-08-23 23:05:38 -0300

    add Orçamento entity with JPA annotations

---
+++ src/main/java/com/couto/OrcaFlow/domin/Orçamento.java
@@ -0,0 +1,33 @@
+package com.couto.OrcaFlow.domin;
+
+import com.couto.OrcaFlow.Enum.StatusOrcamento;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "orcamentos_db")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Orçamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Integer id;
+    private UUID usuarioId;
+    private UUID clienteId;
+    private Long codigoPublico;
+    @Enumerated(EnumType.STRING)
+    private StatusOrcamento statusOrcamento;
+    private LocalDate validade;
+    private String observacao;
+    private BigDecimal total;
+}
