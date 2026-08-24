f552b3620072985079464effdff1540b27312414
cauacouto <cauacouto022@gmail.com>
2026-08-23 23:05:23 -0300

    initialize OrcaFlow project with Spring Boot and Maven wrapper

---
+++ src/main/java/com/couto/OrcaFlow/OrcaFlowApplication.java
@@ -0,0 +1,13 @@
+package com.couto.OrcaFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcaFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcaFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/couto/OrcaFlow/OrcaFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.couto.OrcaFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcaFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
