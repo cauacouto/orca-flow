a1c717115a81eac288ab49f19b97bc1a8afc184a
cauacouto <cauacouto022@gmail.com>
2026-08-23 23:22:07 -0300

    update application properties for testing environment and CI workflow

--- src/test/java/com/couto/OrcaFlow/OrcaFlowApplicationTests.java
+++ src/test/java/com/couto/OrcaFlow/OrcaFlowApplicationTests.java
@@ -2,8 +2,10 @@ package com.couto.OrcaFlow;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("teste")
 class OrcaFlowApplicationTests {
 
 	@Test
