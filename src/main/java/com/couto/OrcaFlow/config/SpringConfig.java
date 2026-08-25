ce8ac40a28295be32f731f9c7503fc0ac06dd202
cauacouto <cauacouto022@gmail.com>
2026-08-25 00:15:38 -0300

    add Spring Security configuration with OAuth2 login and request authorization

---
+++ src/main/java/com/couto/OrcaFlow/config/SpringConfig.java
@@ -0,0 +1,21 @@
+package com.couto.OrcaFlow.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringConfig {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http.authorizeHttpRequests(register-> {
+            register.requestMatchers("/").permitAll();
+            register.anyRequest().authenticated();
+        }).oauth2Login(Customizer.withDefaults())
+                .build();
+    }
+}
