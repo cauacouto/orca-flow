f78a8b508b258de86e3b25116ed349bca599f457
cauacouto <cauacouto022@gmail.com>
2026-08-25 00:15:34 -0300

    add Spring Security OAuth2 client dependencies and enable web security

--- src/main/java/com/couto/OrcaFlow/OrcaFlowApplication.java
+++ src/main/java/com/couto/OrcaFlow/OrcaFlowApplication.java
@@ -2,9 +2,10 @@ package com.couto.OrcaFlow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-
+@EnableWebSecurity
 public class OrcaFlowApplication {
 
 	public static void main(String[] args) {
