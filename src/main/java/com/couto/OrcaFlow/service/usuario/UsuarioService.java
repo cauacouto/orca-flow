0a0a06a1251a689678bfd75cb902e25fe3efb2d3
cauacouto <cauacouto022@gmail.com>
2026-08-25 00:15:53 -0300

    add UsuarioService class for user-related business logic

---
+++ src/main/java/com/couto/OrcaFlow/service/usuario/UsuarioService.java
@@ -0,0 +1,14 @@
+package com.couto.OrcaFlow.service.usuario;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+
+
+
+
+}
