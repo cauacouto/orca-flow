5a42d1ddfd9442f3d74b3248f0ec42ca6ce02ea5
cauacouto <cauacouto022@gmail.com>
2026-08-26 00:46:36 -0300

    add relationships for Cliente and Orcamento in Usuario entity

--- src/main/java/com/couto/OrcaFlow/domin/Usuario.java
+++ src/main/java/com/couto/OrcaFlow/domin/Usuario.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -28,7 +29,15 @@ public class Usuario {
 
     private String name;
 
+    @OneToMany(mappedBy = "usuario")
+    private List<Cliente> clientes;
+
+    @OneToMany(mappedBy = "usuario")
+    private List<Orcamento> orçamentos;
+
     private String picture;
+
+    boolean onboardingCompleted;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
