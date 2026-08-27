b7828a4e950e13ffc58094f0ba2215530f0d1115
cauacouto <cauacouto022@gmail.com>
2026-08-27 19:50:47 -0300

    add OrcamentoMapper mock to OrcamentoTest and refactor test methods

--- src/test/java/com/couto/OrcaFlow/OrcamentoTest.java
+++ src/test/java/com/couto/OrcaFlow/OrcamentoTest.java
@@ -3,8 +3,10 @@ package com.couto.OrcaFlow;
 import com.couto.OrcaFlow.domin.Usuario;
 import com.couto.OrcaFlow.dto.OrcamentoRequest;
 import com.couto.OrcaFlow.dto.OrcamentoResponse;
+import com.couto.OrcaFlow.mapper.OrcamentoMapper;
 import com.couto.OrcaFlow.repository.OrcamentoRepository.OrcamentoRepository;
 import com.couto.OrcaFlow.service.orcamento.OrcamentoService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -20,11 +22,15 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
+@Slf4j
 @ExtendWith(MockitoExtension.class)
 public class OrcamentoTest {
 
     @Mock
     private OrcamentoRepository orcamentoRepository;
+
+  @Mock
+    private OrcamentoMapper orcamentoMapper;
 
     @InjectMocks
     private OrcamentoService orcamentoService;
@@ -42,30 +48,60 @@ public class OrcamentoTest {
 
         Usuario usuario = new Usuario();
         usuario.setId(UUID.randomUUID());
+        UUID idbanco = UUID.randomUUID();
+        String codigo = "tetstetet";
+
 
         UUID clientId = UUID.randomUUID();
         LocalDate data = LocalDate.of(2026,5,6);
 
 
 
-        OrcamentoRequest requets = new OrcamentoRequest(
+        OrcamentoRequest request = new OrcamentoRequest(
                 clientId,
                 data,
                 "tomada"
 
         );
 
+        Orcamento orcamento = new Orcamento();
+
+
+        Mockito.when(orcamentoMapper.toEntity(request))
+                .thenReturn(orcamento);
+
+
+
+
         Mockito.when(orcamentoRepository.save(Mockito.any(Orcamento.class)))
                 .thenAnswer(invocation -> invocation.getArgument(0));
 
-        OrcamentoResponse resultado =
-                orcamentoService.criarOrcamento(requets,usuario);
+
+        OrcamentoResponse response = new OrcamentoResponse(
+                idbanco,
+                codigo,
+                clientId,
+                StatusOrcamento.RASCUNHO,
+                data,
+                "tomada",
+                BigDecimal.ZERO
+        );
+
+
+        Mockito.when(orcamentoMapper.toDto(orcamento))
+                .thenReturn(response);
+
+        OrcamentoResponse resultado = orcamentoService.criarOrcamento(request,usuario);
+
+
 
             Assertions.assertNotNull(resultado);
-            Mockito.verify(orcamentoRepository).save(Mockito.any(Orcamento.class));
+        Assertions.assertEquals(StatusOrcamento.RASCUNHO,resultado.status());
+        Assertions.assertEquals(BigDecimal.ZERO,resultado.valorTotal());
 
-            Assertions.assertEquals(StatusOrcamento.RASCUNHO,resultado.status());
-            Assertions.assertEquals(BigDecimal.ZERO,resultado.valorTotal());
+        Mockito.verify(orcamentoRepository)
+                .save(Mockito.any(Orcamento.class));
+
 
 
 
