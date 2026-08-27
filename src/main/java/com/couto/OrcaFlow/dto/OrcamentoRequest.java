7aea4aa20f6fb6eb7d7b3ffb781195c6dc1be4a5
cauacouto <cauacouto022@gmail.com>
2026-08-27 19:50:37 -0300

    add BigDecimal field to OrcamentoRequest for financial data

--- src/main/java/com/couto/OrcaFlow/dto/OrcamentoRequest.java
+++ src/main/java/com/couto/OrcaFlow/dto/OrcamentoRequest.java
@@ -1,10 +1,12 @@
 package com.couto.OrcaFlow.dto;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record OrcamentoRequest(UUID clientId,
                                LocalDate validade,
                                String observacao
+
                                ) {
 }
