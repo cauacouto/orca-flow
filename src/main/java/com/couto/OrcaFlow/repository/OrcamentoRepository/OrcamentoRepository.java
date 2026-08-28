d2a12edc20f4971baf4c02b07beea852aa637c0d
cauacouto <cauacouto022@gmail.com>
2026-08-27 22:47:39 -0300

    add methods to OrcamentoRepository for user-specific queries

--- src/main/java/com/couto/OrcaFlow/repository/OrcamentoRepository/OrcamentoRepository.java
+++ src/main/java/com/couto/OrcaFlow/repository/OrcamentoRepository/OrcamentoRepository.java
@@ -1,9 +1,15 @@
 package com.couto.OrcaFlow.repository.OrcamentoRepository;
 
 import com.couto.OrcaFlow.domin.Orcamento;
+import com.couto.OrcaFlow.domin.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface OrcamentoRepository extends JpaRepository<Orcamento, UUID> {
+    Optional<Orcamento> findByIdAndUsuario(UUID id, Usuario usuario);
+
+    List<Orcamento> findByUsuario(Usuario usuario);
 }
