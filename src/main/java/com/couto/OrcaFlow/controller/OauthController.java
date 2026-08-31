c4466cb29635a1cffbdc6eadd388692816ae01e3
cauacouto <cauacouto022@gmail.com>
2026-08-30 22:38:08 -0300

    refactor OauthController to return UsuarioDto and integrate UsuarioService

--- src/main/java/com/couto/OrcaFlow/controller/OauthController.java
+++ src/main/java/com/couto/OrcaFlow/controller/OauthController.java
@@ -1,5 +1,10 @@
 package com.couto.OrcaFlow.controller;
 
+import com.couto.OrcaFlow.domin.Usuario;
+import com.couto.OrcaFlow.dto.UsuarioDto;
+import com.couto.OrcaFlow.mapper.UsuarioMapper;
+import com.couto.OrcaFlow.service.usuario.UsuarioService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,15 +12,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class OauthController {
+
+    private final UsuarioService usuarioService;
+    private final UsuarioMapper usuarioMapper;
 
     @RequestMapping("/")
     public String home(){
         return "hello word";
     }
 
-    @GetMapping("/user")
-    public OAuth2User user(@AuthenticationPrincipal OAuth2User user){
-        return user;
-    }
+   @GetMapping("/user")
+   public UsuarioDto user(@AuthenticationPrincipal OAuth2User auth2User){
+     Usuario usuario = usuarioService.buscarOuCriarEntidade(auth2User);
+     return usuarioMapper.toResponse(usuario);
+   }
 }
