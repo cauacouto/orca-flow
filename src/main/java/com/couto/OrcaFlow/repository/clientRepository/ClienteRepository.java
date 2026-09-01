6a5bb2642286c7725b88389cb77ff3f8b09d2b8e
cauacouto <cauacouto022@gmail.com>
2026-08-31 22:02:27 -0300

    add findByIdAndUsuario method to ClienteRepository for enhanced client retrieval

--- src/main/java/com/couto/OrcaFlow/repository/clientRepository/ClienteRepository.java
+++ src/main/java/com/couto/OrcaFlow/repository/clientRepository/ClienteRepository.java
@@ -1,9 +1,12 @@
 package com.couto.OrcaFlow.repository.clientRepository;
 
 import com.couto.OrcaFlow.domin.Cliente;
+import com.couto.OrcaFlow.domin.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ClienteRepository extends JpaRepository<Cliente, UUID> {
+    Optional<Cliente> findByIdAndUsuario(UUID id, Usuario usuario);
 }
